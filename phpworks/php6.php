98f9441c1cce5f6347b5b56f71631dd30417ba94
Athirakrishnanp <athirakrishnanp94@gmail.com>
2017-03-31 10:01:46 -0700

    Update php6.php

--- phpworks/php6.php
+++ phpworks/php6.php
@@ -4,7 +4,7 @@
 * @created date: 24/02/2017
 */
 
-$str = "\"Hello, welcome to \"baabtra\". Baabtra is a division of baabte. It’s one of the leading IT Training institute in Kerala.Baabtra has branches at \"Calicut/Mukkam\",\"Cochin/startup Village\" etc";
+$str = "\"Hello, welcome to \"baabtra\". Baabtra is a division of baabte. It’s one of the leading IT Training institute in Kerala.Baabtra has branches at \"Calicut/Mukkam\",\"Cochin/startup Village\" etc\"";
 echo "$str";
 
 ?>
